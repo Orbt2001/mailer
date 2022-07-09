cb1d449506ae7eb8ccff39d3d99ffd1893898674
Orbt <76572854+Orbt2001@users.noreply.github.com>
2022-07-08 21:38:46 -0300

    Update index.php

--- index.php
+++ index.php
@@ -7,10 +7,10 @@ if($_POST['key']==='orbt'){
     $subject = $_POST['subject'];
     $message = $_POST['fullname'].'<br>'.$_POST['message'] . '<br>' . $_POST['phone'] ;
     $headers = "From:" . $from;
-    if(!mail($to,$subject,$message, $headers)){
+    $ok['ok'] = mail($to,$subject,$message, $headers);
+    if(!$ok){
         throw new Exception("Error Processing your mail", 1);
     }
-    $ok['ok'] = true;
     echo(json_encode($ok));
     http_response_code(200);
 }else{
