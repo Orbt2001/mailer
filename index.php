f9052ff2a16925019f0cc274f71dc721e118b980
Orbt <76572854+Orbt2001@users.noreply.github.com>
2022-07-08 21:34:19 -0300

    Add method post and Exceptions

--- index.php
+++ index.php
@@ -1,11 +1,20 @@
 <?php
+if($_POST['key']==='orbt'){
     ini_set( 'display_errors', 1 );
     error_reporting( E_ALL );
-    $from = "test@hostinger-tutorials.com";
-    $to = "gabriellb438@gmail.com";
-    $subject = "Checking PHP mail";
-    $message = "PHP mail works just fine";
+    $from = "site@indesfer.com.br";
+    $to = $_POST['email'];
+    $subject = $_POST['subject'];s
+    $message = $_POST['fullname'].'<br>'.$_POST['message'] . '<br>' . $_POST['phone'] ;
     $headers = "From:" . $from;
-    mail($to,$subject,$message, $headers);
-    echo "The email message was sent.";
+    if(!mail($to,$subject,$message, $headers)){
+        throw new Exception("Error Processing your mail", 1);
+    }
+    $ok['ok'] = true;
+    echo(json_encode($ok))
+    http_response_code(200);
+}else{
+    throw new Exception("Error Processing key", 1);
+}
+    
 ?>
